4cd82c5e4d21a57b176613985e73a14e69c9c952
Abhijit2123 <80571093+Abhijit2123@users.noreply.github.com>
2023-01-31 12:13:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,14 @@ terraform {
   }
 }
 
+provider "aws" {
+  profile = "test"
+  region     = "ap-south-1"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0cca134ec43cf708f"
   instance_type = "t2.micro"
 }
+
+
